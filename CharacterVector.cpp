16b323642954502bf55ab38a9a2a796f026326d8
Vadim Kudlay <vadim.kudlay@richmond.edu>
2018-10-03 09:45:09 -0400

    Added CharacterVector functionality, made respective tests in VectorTester

--- CharacterVector.cpp
+++ CharacterVector.cpp
@@ -8,39 +8,48 @@
 CharacterVector::CharacterVector()  {}
 CharacterVector::~CharacterVector() {}
 
-int CharacterVector::size() 
-{  
-   return ...
+int CharacterVector::size()
+{
+   return characterVector.size();
 }
 
 // just return the character at the specified index; use the at() method rather
 // than [] so that an out_of_range error will be thrown for an invalid index
-char CharacterVector::get(int index) 
-{ 
-   return ...
+char CharacterVector::get(int index)
+{
+   return characterVector.at(index);
 }
 
 // if index is size-legitimate, put the value at that index;
 // otherwise, use push_back to append to the end of the vector
 void CharacterVector::put(char value, int index)
 {
+  if(index >= this->size())
+    return this->put(value);
+
+  characterVector.at(index) = value;
 }
 
 // use push_back to append
 void CharacterVector::put(char value)
 {
+  characterVector.push_back(value);
 }
 
 // for each integer in integerVector, use static_cast<char> to append as a
 // character to characterVector
 void CharacterVector::appendIntegerVector(IntegerVector& integerVector)
 {
+  // for(int i = 0; i < integerVector.size(); ++i)
+  //   characterVector.put(static_cast<char>(integerVector.get(i)))
 }
 
 // for each double in doubleVector, use static_cast<char> to append as a
 // character to characterVector
 void CharacterVector::appendDoubleVector(DoubleVector& doubleVector)
 {
+  // for(int i = 0; i < integerVector.size(); ++i)
+  //   characterVector.put(static_cast<char>(integerVector.get(i)))
 }
 
 #endif
--- VectorTester.cpp
+++ VectorTester.cpp
@@ -3,69 +3,129 @@
 #include "DoubleVector.h"
 #include "CharacterVector.h"
 
+std::string toString(CharacterVector v);
+
 int main()
 {
-   IntegerVector   iv;
-   DoubleVector    dv;
-   CharacterVector cv;
+  IntegerVector   iv;
+  DoubleVector    dv;
+  CharacterVector cv;
 
-   //-------------------------------------------------------------------------
+  //-------------------------------------------------------------------------
 
-   // test IntegerVector: put, get, size, out_of_range
+  // test IntegerVector: put, get, size, out_of_range
 
-   std::cout << "--------------" << std::endl;
-   std::cout << "IntegerVector:" << std::endl;
-   std::cout << "--------------" << std::endl;
+  std::cout << "--------------" << std::endl;
+  std::cout << "IntegerVector:" << std::endl;
+  std::cout << "--------------" << std::endl;
 
-   //-------------------------------------------------------------------------
+  //-------------------------------------------------------------------------
 
-   // test CharacterVector: put, get, size, out_of_range
+  // test CharacterVector: put, get, size, out_of_range
 
-   std::cout << std::endl;
-   std::cout << "----------------" << std::endl;
-   std::cout << "CharacterVector:" << std::endl;
-   std::cout << "----------------" << std::endl;
+  std::cout << std::endl;
+  std::cout << "----------------" << std::endl;
+  std::cout << "CharacterVector:" << std::endl;
+  std::cout << "----------------" << std::endl;
 
-   //-------------------------------------------------------------------------
+  std::cout << "+++++++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
 
-   // test DoubleVector: put, get, size, out_of_range
+  // Testing CharacterVector
+  std::cout << std::endl << "Testing CharVector:" << std::endl;
 
-   std::cout << std::endl;
-   std::cout << "-------------" << std::endl;
-   std::cout << "DoubleVector:" << std::endl;
-   std::cout << "-------------" << std::endl;
+  std::cout << "\tSize    : " << cv.size()    << " [0]" << std::endl;
+  std::cout << "\tContent : " << toString(cv) << " []" << std::endl;
+  try {
+    std::cout << "\tGet(0) : " << cv.get(0) << " [N/A]";
+  } catch(const std::out_of_range& e) {
+    std::cerr << "Out Of Range Exception thrown: " << e.what() << std::endl;
+  }
 
-   //-------------------------------------------------------------------------
+  // Testing non-empty CharacterVector
+  cv.put('a');
+  cv.put('b');
+  cv.put('c');
 
-   // using empty IntegerVector, test appending cv & dv from above 
-   IntegerVector iv2;
+  std::cout << "\n\tSize    : " << cv.size()    << " [3]" << std::endl;
+  std::cout << "\tContent : " << cv.get(1) << " [b]" << std::endl << std::endl;
 
-   std::cout << std::endl;
-   std::cout << "--------------------------" << std::endl;
-   std::cout << "appended-to IntegerVector:" << std::endl;
-   std::cout << "--------------------------" << std::endl;
+  cv.put('d');
+  cv.put('e');
 
-   //-------------------------------------------------------------------------
+  std::cout << "\tSize    : " << cv.size()    << " [5]" << std::endl;
+  std::cout << "\tContent : " << toString(cv) << " [a, b, c, d, e]" << std::endl;
 
-   // using empty CharacterVector, test appending iv & dv from above
-   CharacterVector cv2;
+  // Testing non-empty
+  std::cout << std::endl << "Testing out of range:" << std::endl;
 
-   std::cout << std::endl;
-   std::cout << "----------------------------" << std::endl;
-   std::cout << "appended-to CharacterVector:" << std::endl;
-   std::cout << "----------------------------" << std::endl;
+  std::cout << "\tSize    : " << cv.size()    << " [5]" << std::endl;
+  try {
+    std::cout << "\tGet(6)  : " << cv.get(6) << " [N/A]"
+    << std::endl << std::endl;
+  } catch(const std::out_of_range& e) {
+    std::cerr << "Out Of Range Exception thrown: " << e.what() << std::endl;
+  }
 
-   //-------------------------------------------------------------------------
+  std::cout << "\n\tSize    : " << cv.size()    << " [5]" << std::endl;
+  try {
+    std::cout << "\tGet(-1) : " << cv.get(-1) << " [N/A]"
+    << std::endl << std::endl;
+  } catch(const std::out_of_range& e) {
+    std::cerr << "Out Of Range Exception thrown: " << e.what() << std::endl;
+  }
+  std::cout << "\n+++++++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
 
-   // using empty DoubleVector, test appending iv & cv from above
-   DoubleVector dv2;
+  //-------------------------------------------------------------------------
 
-   std::cout << std::endl;
-   std::cout << "-------------------------" << std::endl;
-   std::cout << "appended-to DoubleVector:" << std::endl;
-   std::cout << "-------------------------" << std::endl;
+  // test DoubleVector: put, get, size, out_of_range
 
-   //-------------------------------------------------------------------------
+  std::cout << std::endl;
+  std::cout << "-------------" << std::endl;
+  std::cout << "DoubleVector:" << std::endl;
+  std::cout << "-------------" << std::endl;
 
-   return 0;
+  //-------------------------------------------------------------------------
+
+  // using empty IntegerVector, test appending cv & dv from above
+  IntegerVector iv2;
+
+  std::cout << std::endl;
+  std::cout << "--------------------------" << std::endl;
+  std::cout << "appended-to IntegerVector:" << std::endl;
+  std::cout << "--------------------------" << std::endl;
+
+  //-------------------------------------------------------------------------
+
+  // using empty CharacterVector, test appending iv & dv from above
+  CharacterVector cv2;
+
+  std::cout << std::endl;
+  std::cout << "----------------------------" << std::endl;
+  std::cout << "appended-to CharacterVector:" << std::endl;
+  std::cout << "----------------------------" << std::endl;
+
+  //-------------------------------------------------------------------------
+
+  // using empty DoubleVector, test appending iv & cv from above
+  DoubleVector dv2;
+
+  std::cout << std::endl;
+  std::cout << "-------------------------" << std::endl;
+  std::cout << "appended-to DoubleVector:" << std::endl;
+  std::cout << "-------------------------" << std::endl;
+
+  //-------------------------------------------------------------------------
+
+  return 0;
+}
+
+std::string toString(CharacterVector v){
+  std::string out("{");
+  for(int i = 0; i < v.size(); ++i){
+    out += v.get(i);
+    if(i < v.size()-1)
+    out += ", ";
+  }
+  out += "}";
+  return out;
 }
